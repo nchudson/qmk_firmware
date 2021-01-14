379d8bf9e732e4a2105379aee1a2356d218b738b
Nathaniel Hudson <nchudson18@gmail.com>
2021-01-14 02:01:21 -0800

    Bluetooth kinda works

--- keyboards/planck/rev4/config.h
+++ keyboards/planck/rev4/config.h
@@ -4,3 +4,14 @@
 #define AdafruitBleResetPin D3
 #define AdafruitBleCSPin    D1
 #define AdafruitBleIRQPin   D2
+/*
+#define SPI_DRIVER          SPID1
+
+#define SPI_SCK_PIN         B1
+#define SPI_MISO_PIN        B3
+#define SPI_MOSI_PIN        B2
+
+#define SPI_SCK_PAL_MODE    5
+#define SPI_MISO_PAL_MODE   5
+#define SPI_MOSI_PAL_MODE   5
+*/
--- keyboards/planck/rev4/rules.mk
+++ keyboards/planck/rev4/rules.mk
@@ -18,7 +18,7 @@ BOOTLOADER = atmel-dfu
 BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = no       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = yes         # Console for debug
+CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no        # Commands for debug and configuration
 NKRO_ENABLE = no            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
@@ -34,3 +34,4 @@ SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
 LAYOUTS = ortho_4x12 planck_mit
 LAYOUTS_HAS_RGB = no
+
